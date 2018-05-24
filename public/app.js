0245fa42a28575f3072e57c6d006422ee71a200b
mai <168496714@qq.com>
2018-05-24 13:27:14 +0800

    Init project

---
+++ public/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+// import your component and use in here
+import HelloWorld from "../src/component";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <HelloWorld />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
---
+++ src/component/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import'../css/instagram.min.css'
+
+export default class Wrapper extends Component {
+  render() {
+    return (
+      <figure className='filter-1977'>
+        <img src='http://orscxqn8h.bkt.clouddn.com/18-3-3/943334.jpg' alt='test' width={500} height={500} />
+      </figure>
+    )
+  }
+}
